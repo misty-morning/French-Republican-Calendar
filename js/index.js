c50f99cc7b791275a81964e6a6208639dda4b97b
andrey_kharchenko <monk_of_emptiness@mail.ru>
2015-06-04 00:56:15 +0300

    rev calendar is a function

--- js/index.js
+++ js/index.js
@@ -24,68 +24,57 @@ $(document).ready(function() {
 
 	//Revolutionary Calendar
 
-/*
-	var year = {
-		january: 31,
-		february: 28,
-		march: 31,
-		april: 30,
-		may: 31,
-		june: 30,
-		july: 31,
-		august: 31,
-		september: 30,
-		october: 31,
-		november: 30,
-		december: 31
-	};
-*/	
+	function revolutionaryCalendar(firstDayDate, date) {
+		var thisCommonMonth = date.getMonth();
+		var thisCommonDay = date.getDate();
+
+		var firstDayOfThisYear = firstDayDate.getDate();
+		var firstMonthOfThisYear = firstDayDate.getMonth();
+
+		var daysAmount;
+
+		var year = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+		if ((thisCommonMonth != firstMonthOfThisYear) || (thisCommonMonth === firstMonthOfThisYear && thisCommonDay < firstDayOfThisYear)) {
+			daysAmount = year[firstMonthOfThisYear] - firstDayOfThisYear;
+			if (thisCommonMonth > firstMonthOfThisYear) {
+				for (var i = firstMonthOfThisYear + 1; i < thisCommonMonth; i++) {
+					daysAmount += year[i];
+				}
+				daysAmount += thisCommonDay;
+			}
+			else if (thisCommonMonth <= firstMonthOfThisYear) {
+				for (var i = firstMonthOfThisYear + 1; i <= 11; i++) {
+					daysAmount += year[i];
+				}
+				for (var i = 0; i < thisCommonMonth; i++) {
+					daysAmount += year[i];
+				}
+				daysAmount += thisCommonDay;
+			}
+		}
+		else {
+			daysAmount = thisCommonDay - firstDayOfThisYear;
+		}
+		//daysAmount += 1;
+		
+		var revolutionaryDate = {};
+		
+		revolutionaryDate.year = date.getFullYear() - 1791;
+		revolutionaryDate.month = daysAmount / 30 >> 0;	
+		revolutionaryDate.day = daysAmount % 30;
+		revolutionaryDate.decade = daysAmount % 10 - 1;
+
+		console.log("daysAmount: " + daysAmount);
+
+		return revolutionaryDate;
+	}
+
 	var testTime = new Date(2015, 4, 20);
 	var firstDateOfThisYear = new Date(2014, 8, 23);
 
-	
-	var thisCommonMonth = commonTime.getMonth();
-	var thisCommonDay = commonTime.getDate();
+	var revTime = revolutionaryCalendar(firstDateOfThisYear, commonTime);
 
-	
-	var firstDayOfThisYear = firstDateOfThisYear.getDate();
-	var firstMonthOfThisYear = firstDateOfThisYear.getMonth();
-
-	var revTime = {};
-
-	var daysAmount;
-
-	var year = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-	if ((thisCommonMonth != firstMonthOfThisYear) || (thisCommonMonth === firstMonthOfThisYear && thisCommonDay < firstDayOfThisYear)) {
-		daysAmount = year[firstMonthOfThisYear] - firstDayOfThisYear;
-		if (thisCommonMonth > firstMonthOfThisYear) {
-			for (var i = firstMonthOfThisYear + 1; i < thisCommonMonth; i++) {
-				daysAmount += year[i];
-			}
-			daysAmount += thisCommonDay;
-		}
-		else if (thisCommonMonth <= firstMonthOfThisYear) {
-			for (var i = firstMonthOfThisYear + 1; i <= 11; i++) {
-				daysAmount += year[i];
-			}
-			for (var i = 0; i < thisCommonMonth; i++) {
-				daysAmount += year[i];
-			}
-			daysAmount += thisCommonDay;
-		}
-	}
-	else {
-		daysAmount = thisCommonDay - firstDayOfThisYear;
-	}
-	//daysAmount += 1;
-	revTime.year = commonTime.getFullYear() - 1791;
-	revTime.month = daysAmount / 30 >> 0;	
-	revTime.day = daysAmount % 30;
-	revTime.decade = daysAmount % 10 - 1;
-
-
-	console.log("daysAmount: " + daysAmount);
 	console.log("monthIndex: " + revTime.month);
 
 	var revMonthsArray = ["вандемьера", "брюмера", "фримера", "нивоза", "плювиоза", "вантоза", "жерминаля", "флореаля", "прериаля", "мессидора", "термидора", "фрюктидора"];
