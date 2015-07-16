c643b44d730861755f88b7fa7d3a0762fb55861d
andrey_kharchenko <monk_of_emptiness@mail.ru>
2015-07-16 23:43:25 +0300

    whole new structure

--- js/index.js
+++ js/index.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
-	// Ui
+	//Ui
+	var $indexCommonCalendar = $("#index--common-calendar");
 	var $indexRevCalendar = $("#index--rev-calendar");
 
+	var commonTime = new Date();
+	renderCommonCalendar(commonTime, $indexCommonCalendar)
+	
+	var revTime = new RevolutionaryCalendar(commonTime, 23);
 	renderRevCalendar(revTime, $indexRevCalendar);
+
+	// Calculate rev date
+
+	//Ui
+	$calcDay = $("#calc--day");
+	$calcMonth = $("#calc-month");
+	$calcYear = $("#calc--year");
+	$calcFirstDay = $("#calc--first-day");
+	$calcBnt = $("#calc--btn");
+	$calcResult = $("#calc--result");
+
+	$calcBnt.click(function() {
+		var date = new Date($calcYear.val(), $calcMonth.val(), $calcDay.val());
+
+		var revDate = new RevolutionaryCalendar(date, $calcFirstDay.val());
+		renderRevCalendar(revDate, $calcResult);
+	});
 });
 
