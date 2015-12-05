d56a627ab3c5144beafdcce05e9d5db5f3781413
andrey_kharchenko <monk_of_emptiness@mail.ru>
2015-12-05 23:02:04 +0300

    months changing

--- js/index.js
+++ js/index.js
@@ -31,30 +31,65 @@ $(document).ready(function() {
 	for (var i = 0; i < 12; i++) {
 		$("#vc--months").append("<div data-id='" + i + "' class='vc--month-btn'>"+ calendarNames.monthCommon[i] +"</div>");
 	};
-	$("#vc--months").append("<div data-id='12' class='vc--month-btn'>Санкюлотиды</div>");
+	$("#vc--months").append("<div data-id='12' data-month-btn class='vc--month-btn'>Санкюлотиды</div>");
 
-	$("#vc--months [data-id='" + revMouthIndex + "']").addClass("__current-month");
+	$("#vc--months [data-id='" + revMouthIndex + "']").addClass("__current-month").attr("title", "Текущий месяц");
 
 	$visualCalendar.append("<div id='vc--current-month'></div>");
-	$("#vc--current-month").append("<div id='vc--head-row'></div>");
-	for(var i = 0; i < 10; i++) {
-		$("#vc--head-row").append("<div class='vc--head-cell'><div class='vc--head-text'>"+ calendarNames.decade[i] +"</div></div>");
-	}
 
-	var vcId = 1;
-	for(var i = 0; i < 3; i++) {
-		$("#vc--current-month").append("<div data-id='" + i + "' class='vc-row'></div>");
-		for(var j = 0; j < 10; j++) {
-			$(".vc-row[data-id='" + i + "']").append("<div data-id='" + vcId + "' class='vc-cell' data-toggle='tooltip' data-placement='bottom' title='День "+ calendarNames.day[revMouthIndex * 30 + vcId - 1] +"'><div>"+ vcId +"</div><div class='vc-cell-day-name'>День<br>"+ calendarNames.day[revMouthIndex * 30 + vcId - 1] +"</div></div>");
-			if (j === 3 || j === 8 || j === 9) {
-				$(".vc-cell[data-id='" + vcId + "']").addClass("__holyday");
-			}
-			vcId++;
+/*	if (revTime.dayName < 360) {
+		$("#vc--current-month").append("<div id='vc--head-row'></div>");
+		for(var i = 0; i < 10; i++) {
+			$("#vc--head-row").append("<div class='vc--head-cell'><div class='vc--head-text'>"+ calendarNames.decade[i] +"</div></div>");
 		}
+
+		var vcId = 1;
+		for(var i = 0; i < 3; i++) {
+			$("#vc--current-month").append("<div data-id='" + i + "' class='vc-row'></div>");
+			for(var j = 0; j < 10; j++) {
+				$(".vc-row[data-id='" + i + "']").append("<div data-id='" + vcId + "' class='vc-cell' data-toggle='tooltip' data-placement='bottom' title='День "+ calendarNames.day[revMouthIndex * 30 + vcId - 1] +"'><div>"+ vcId +"</div><div class='vc-cell-day-name'>День<br>"+ calendarNames.day[revMouthIndex * 30 + vcId - 1] +"</div></div>");
+				if (j === 3 || j === 8 || j === 9) {
+					$(".vc-cell[data-id='" + vcId + "']").addClass("__holyday");
+				}
+				vcId++;
+			}
+		}
+
+		
 	}
+	else {
 
-	$(".vc-cell[data-id='" + revTime.day + "']").addClass("__active-cell");
+	}
+	$(".vc-cell[data-id='" + revTime.day + "']").addClass("__active-cell");*/
 
+	$(".vc--month-btn").click(function() {
+		$(".vc--month-btn").removeClass("__active-month");
+		$(this).addClass("__active-month");
+
+		$("#vc--current-month").empty();
+		$("#vc--current-month").append("<div id='vc--head-row'></div>");
+		for(var i = 0; i < 10; i++) {
+			$("#vc--head-row").append("<div class='vc--head-cell'><div class='vc--head-text'>"+ calendarNames.decade[i] +"</div></div>");
+		}
+
+		var activeMonthFirstDay = $(this).data("id") * 30 - 1;
+		var vcId = 1;
+		for(var i = 0; i < 3; i++) {
+			$("#vc--current-month").append("<div data-id='" + i + "' class='vc-row'></div>");
+			for(var j = 0; j < 10; j++) {
+				$(".vc-row[data-id='" + i + "']").append("<div data-id='" + vcId + "' class='vc-cell' data-toggle='tooltip' data-placement='bottom' title='День "+ calendarNames.day[activeMonthFirstDay + vcId] +"'><div>"+ vcId +"</div><div class='vc-cell-day-name'>День<br>"+ calendarNames.day[activeMonthFirstDay + vcId] +"</div></div>");
+				if (j === 3 || j === 8 || j === 9) {
+					$(".vc-cell[data-id='" + vcId + "']").addClass("__holyday");
+				}
+				vcId++;
+			}
+		}
+		if ($(this).data("id") == revMouthIndex) {
+			$(".vc-cell[data-id='" + revTime.day + "']").addClass("__active-cell");
+		}
+
+	});
+	$(".vc--month-btn[data-id='" + revMouthIndex + "']").trigger("click");
 
 	// Date calculation
 
