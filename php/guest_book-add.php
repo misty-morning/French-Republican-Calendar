129df7d513e0645b0c136c1bfd6a07c58b475f0a
harchenko_aa <harchenko_aa@sapr.tvz.ru>
2016-05-17 09:10:18 +0300

    adding works. angular http fix

--- php/guest_book-add.php
+++ php/guest_book-add.php
@@ -1,15 +1,18 @@
 <?php
 	include 'db_connect.php';
+	header('Content-Type: application/json');
 
 	$name = $_POST['name'];
 	$record = $_POST['text'];
 
-	$query = "INSERT INTO guest_book (name, record) VALUES ('".$name."', '".$record."')";
+	//$query = "INSERT INTO guest_book (name, record) VALUES ('$name', '$record')";
+	$query = "INSERT INTO guest_book (name, record) VALUES ('$name', '$record')";
+
 	mysqli_query($dbc, $query);
 
 	$id = mysqli_insert_id($dbc);
 	$answer = array('id' => $id);
-
+	//array_push($answer, 'name' => $_POST['name']);
 	echo json_encode($answer);
 	mysqli_close($dbc);
 
