b6b36ebad65e8238d726bd3a5c842092c3f9e7f1
harchenko_aa <harchenko_aa@sapr.tvz.ru>
2016-05-18 13:24:47 +0300

    php optimization

--- php/guest_book-add.php
+++ php/guest_book-add.php
@@ -3,32 +3,18 @@
 	header('Content-Type: application/json');
 
 	$name = $_POST['name'];
-	$record = $_POST['text'];
+	$text = $_POST['text'];
 
-	$query = "INSERT INTO guest_book (name, record) VALUES ('$name', '$record')";
+	$query = "INSERT INTO guest_book (name, text) VALUES ('$name', '$text')";
 	mysqli_query($dbc, $query);
 
 	$id = mysqli_insert_id($dbc);
 
-	$new_record_query = "SELECT id, name, record, DATE_FORMAT(time, '%d.%m.%Y %H:%i') time FROM guest_book WHERE id=$id LIMIT 1";
+	$new_record_query = "SELECT id, name, text, DATE_FORMAT(time, '%d.%m.%Y %H:%i') time FROM guest_book WHERE id=$id LIMIT 1";
 	$new_record = mysqli_query($dbc, $new_record_query);
-	//$records = array();
-	class record {
-		public $id;
-		public $time;
-		public $name;
-		public $text;
-	}
-	while ($row = mysqli_fetch_array($new_record)) {
-		$tmp = new record();
-		$tmp->id = $row['id'];
-		$tmp->time = $row['time'];
-		$tmp->name = $row['name'];
-		$tmp->text = $row['record'];
-		//array_push($records, $tmp);
-	}
+	$new_record_answer = mysqli_fetch_array($new_record);
 
-	echo json_encode($tmp);
+	echo json_encode($new_record_answer);
 	mysqli_close($dbc);
 
 ?>
--- php/guest_book-get.php
+++ php/guest_book-get.php
@@ -1,24 +1,12 @@
 <?php
 	include 'db_connect.php';
 
-	//$records_query = "SELECT * FROM guest_book ORDER BY id DESC";
-	$records_query = "SELECT id, name, record, DATE_FORMAT(time, '%d.%m.%Y %H:%i') time FROM guest_book";
+	$records_query = "SELECT id, name, text, DATE_FORMAT(time, '%d.%m.%Y %H:%i') time FROM guest_book";
 
 	$records_result = mysqli_query($dbc, $records_query);
 	$records = array();
-	class record {
-		public $id;
-		public $time;
-		public $name;
-		public $text;
-	}
 	while ($row = mysqli_fetch_array($records_result)) {
-		$tmp = new record();
-		$tmp->id = $row['id'];
-		$tmp->time = $row['time'];
-		$tmp->name = $row['name'];
-		$tmp->text = $row['record'];
-		array_push($records, $tmp);
+		array_push($records, $row);
 	}
 	echo json_encode($records);
 
